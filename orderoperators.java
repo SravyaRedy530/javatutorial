e785e9083e2c91ffc3959f013839af65de5e2e5b
SravyaRedy530 <sravyaveeramreddy530@gmail.com>
2025-01-03 18:51:50 +0530

    Add files via upload

---
+++ orderoperators.java
@@ -0,0 +1,8 @@
+public class orderoperators {
+    public static void main(String[] args) {
+        //ORDER OF OPERATIONS [P-E-M-D-A-S]
+        double result = 3 + 4 * 2 / 2.0;
+        System.out.println(result);
+    }
+    
+}
