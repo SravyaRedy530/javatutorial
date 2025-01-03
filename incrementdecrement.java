18367a867e8e162cd7e2d5fa5c3ae7c5332e8bca
SravyaRedy530 <sravyaveeramreddy530@gmail.com>
2025-01-03 18:52:14 +0530

    Add files via upload

---
+++ incrementdecrement.java
@@ -0,0 +1,10 @@
+public class incrementdecrement {
+    public static void main(String[] args) {
+        //increment and Drecrement operators
+        int x=1;
+        x++;
+        x--;
+        System.out.println(x);
+    }
+    
+}
