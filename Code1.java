1e4d91972fda72abd054738bf306cc9834b7943f
SravyaRedy530 <sravyaveeramreddy530@gmail.com>
2025-01-03 17:00:51 +0530

    Add files via upload

---
+++ Code1.java
@@ -0,0 +1,15 @@
+public class Code1 {
+    public static void main(String[] args) {
+        int age=21;
+        System.out.println(age);
+        int year=2025;
+        System.out.println("The year is " + year);
+        System.out.println("The year is " + year);
+        double price=19.99;
+        System.out.println("$" + price);
+        char grade = 'A';
+        System.out.println(grade);
+        boolean isStudent = true;
+        System.out.println(isStudent);
+    }
+}
