f3e7c5ba83d1a6a84fd36814f0d6d609fbe88686
SravyaRedy530 <sravyaveeramreddy530@gmail.com>
2025-01-03 17:07:23 +0530

    Add files via upload
    If Else Statements

---
+++ code2.java
@@ -0,0 +1,16 @@
+public class code2 {
+    public static void main(String[] args) {
+        boolean isStudent = true;
+        if(isStudent){
+            System.out.println("You are a student!");
+        }else{
+            System.out.println("You are NOT a student!");
+        }
+        
+        
+
+
+
+    }
+    
+}
